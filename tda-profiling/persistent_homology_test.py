d0c3271128965e4517e0a932dc59ba0e11638718
Ben Swift <ben@benswift.me>
2025-01-24 08:59:33 +0800

    add gudhi code for persistent homology

--- tda-profiling/persistent_homology_test.py
+++ tda-profiling/persistent_homology_test.py
@@ -2,6 +2,7 @@
 # requires-python = ">=3.8,<3.12"
 # dependencies = [
 #     "giotto-ph",
+#     "gudhi",
 #     "numpy",
 # ]
 # ///
@@ -12,12 +13,13 @@
 #
 import time
 
+import gudhi
 import numpy as np
 from gph import ripser_parallel
 
 MAX_DIM = 10
 MAX_POINTS = 1000
-TDA_LIB = "giotto"
+TDA_LIB = "gudhi" # Options: "giotto", "gudhi"
 
 # Print CSV header
 print("ndim,npoints,time")
@@ -28,6 +30,10 @@ for ndim in range(2, MAX_DIM, 1):
         start_time = time.time()
         if TDA_LIB == "giotto":
             dgm = ripser_parallel(pc, maxdim=2, n_threads=-1)
+        elif TDA_LIB == "gudhi":
+            rips = gudhi.RipsComplex(points=pc)
+            st = rips.create_simplex_tree(max_dimension=2)
+            dgm = st.persistence()
         else:
             raise ValueError(f"Unsupported TDA library: {TDA_LIB}")
         end_time = time.time()
