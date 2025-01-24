403e00edcbf0a207de67c9fd8aaeb72724c51d10
Ben Swift <ben@benswift.me>
2025-01-24 08:56:02 +0800

    prepare to make TDA benchmarking code multi-lib

--- tda-profiling/persistent_homology_test.py
+++ tda-profiling/persistent_homology_test.py
@@ -10,13 +10,14 @@
 #
 #     uv run persistent_homology_test.py
 #
+import time
+
 import numpy as np
 from gph import ripser_parallel
 
-import time
-
 MAX_DIM = 10
 MAX_POINTS = 1000
+TDA_LIB = "giotto"
 
 # Print CSV header
 print("ndim,npoints,time")
@@ -25,7 +26,10 @@ for ndim in range(2, MAX_DIM, 1):
         pc = np.random.random((npoints, ndim))
 
         start_time = time.time()
-        dgm = ripser_parallel(pc, maxdim=2, n_threads=-1)
+        if TDA_LIB == "giotto":
+            dgm = ripser_parallel(pc, maxdim=2, n_threads=-1)
+        else:
+            raise ValueError(f"Unsupported TDA library: {TDA_LIB}")
         end_time = time.time()
 
         total_time = end_time - start_time
