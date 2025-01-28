2c53be016e86953341f1170228240d4cb6eac895
sungyeon <24203738+YeoniH@users.noreply.github.com>
2025-01-28 12:19:39 +1100

    Update tda_aesop.py

--- fake-panic/workspace/tda_aesop.py
+++ fake-panic/workspace/tda_aesop.py
@@ -15,8 +15,8 @@ from gph import ripser_parallel
 run_panic = Panic()
 TDA_LIB = "gudhi"  # "gudhi", "giotto"
 MAX_DIM = 2
-num_iterations = 20
-nseed = 3
+num_iterations = 100
+nseed = 5
 
 os.system("mkdir aesop aesop/persistence_dgm aesop/point_cloud")
 folder = os.path.join("aesop/extract")
@@ -35,45 +35,45 @@ if os.path.isdir(folder):
                 file_name = f"{f[:-4]}_line{count}_seed{seed}"
                 print(file_name)
 
-                # point_cloud = []
-                # # output_data = []
-                # for i in range(int(num_iterations / 2)):
-                #     # print(f"Sequence number {2 * i}")
-                #     image_url = run_panic.generate_image(prompt)
-                #     print(f"Image {2 * i}: {image_url}")
-                #
-                #     image_embedding = run_panic.calculate_embedding("vision", image_url)
-                #     point_cloud.append(image_embedding)
-                #     # output_data.append(
-                #     #     {
-                #     #         "seq_no": 2 * i,
-                #     #         "type": "image",
-                #     #         "input": prompt,
-                #     #         # "embedding": image_embedding,
-                #     #     }
-                #     # )
-                #
-                #     # BLIP prefixes the string with "Caption: " - remove this
-                #     caption = run_panic.caption_image(image_url)
-                #     if caption.startswith("Caption: "):
-                #         caption = caption[9:]
-                #     print(f"Caption {2 * i + 1}: {caption}")
-                #
-                #     caption_embedding = run_panic.calculate_embedding("text", caption)
-                #     point_cloud.append(caption_embedding)
-                #     prompt = caption
-                # np.savetxt(f"aesop/point_cloud/{file_name}.txt", point_cloud, fmt="%f")#"%.6f")
-                #
-                #
-                # rips = RipsComplex(points=point_cloud)  # , max_edge_length=10)
-                # simplex_tree = rips.create_simplex_tree(max_dimension=3)
-                # diagram = simplex_tree.persistence(homology_coeff_field=2, min_persistence=0)
-                #
-                # ax = plot_persistence_diagram(diagram)
-                # ax.set_title(prompt)
-                # ax.set_aspect("equal")
-                # # plt.show()
-                # plt.savefig(f"aesop/persistence_dgm/PD_{file_name}.png")
-                # plt.close()
-                
+                point_cloud = []
+                # output_data = []
+                for i in range(int(num_iterations / 2)):
+                    # print(f"Sequence number {2 * i}")
+                    image_url = run_panic.generate_image(prompt)
+                    print(f"Image {2 * i}: {image_url}")
+
+                    image_embedding = run_panic.calculate_embedding("vision", image_url)
+                    point_cloud.append(image_embedding)
+                    # output_data.append(
+                    #     {
+                    #         "seq_no": 2 * i,
+                    #         "type": "image",
+                    #         "input": prompt,
+                    #         # "embedding": image_embedding,
+                    #     }
+                    # )
+
+                    # BLIP prefixes the string with "Caption: " - remove this
+                    caption = run_panic.caption_image(image_url)
+                    if caption.startswith("Caption: "):
+                        caption = caption[9:]
+                    print(f"Caption {2 * i + 1}: {caption}")
+
+                    caption_embedding = run_panic.calculate_embedding("text", caption)
+                    point_cloud.append(caption_embedding)
+                    prompt = caption
+                np.savetxt(f"aesop/point_cloud/{file_name}.txt", point_cloud, fmt="%f")#"%.6f")
+
+
+                rips = RipsComplex(points=point_cloud)  # , max_edge_length=10)
+                simplex_tree = rips.create_simplex_tree(max_dimension=3)
+                diagram = simplex_tree.persistence(homology_coeff_field=2, min_persistence=0)
+
+                ax = plot_persistence_diagram(diagram)
+                ax.set_title(prompt)
+                ax.set_aspect("equal")
+                # plt.show()
+                plt.savefig(f"aesop/persistence_dgm/PD_{file_name}.png")
+                plt.close()
+
             count += 1
